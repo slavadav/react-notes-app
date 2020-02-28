7341d1fc063a906d39ad81d49a9a0ef3236aab52
Слава <slava@MacBook-Air-apple.local>
2020-02-28 21:25:13 +0300

    added react-notes-app

---
+++ src/components/Form.js
@@ -0,0 +1,41 @@
+import React, {useState, useContext} from 'react'
+import { AlertContext } from '../context/alert/alertContext';
+import { FirebaseContext } from '../context/firebase/firebaseContext';
+
+export const Form = () => {
+    const [value, setValue] = useState('')
+    const alert = useContext(AlertContext)
+    const firebase = useContext(FirebaseContext)
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        if (value.trim()) {
+            firebase.addNote(value.trim()).then(() => {
+                alert.show('Note was created.', 'success')
+            }).catch(() => {
+                alert.show('Something error.', 'danger')
+            })
+            
+            setValue('')
+        } else {
+            alert.show('Insert note text.', 'warning')
+        }
+
+        
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="form-group">
+                <input 
+                    type="text" 
+                    className="form-control" 
+                    placeholder="Enter Note title"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                />
+            </div>
+        </form>
+    )
+}
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">React Notes App</h1>
+            <p className="lead">
+                Note App for study only.
+            </p>
+        </div>
+    </div>
+)
